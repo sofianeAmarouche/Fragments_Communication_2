a4aa7ca5728a8696d8966dd3d9d8b35b80f2959f
sofianeAmarouche <azroubar@gmail.com>
2019-02-04 21:54:18 -0500

    first commit

---
+++ app/src/main/java/com/example/sofiane/passing_data_between_fragments/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sofiane.passing_data_between_fragments;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements Fragment_one.SendMessage{
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void sendData(String message) {
+        String tag = "android:switcher:" + R.id.viewPager + ":" + 1;
+        Fragment_Two f = (Fragment_Two) getSupportFragmentManager().findFragmentByTag(tag);
+        f.displayReceivedData(message);
+    }
+}
---
+++ app/src/main/java/com/example/sofiane/passing_data_between_fragments/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.sofiane.passing_data_between_fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        if (position == 0) {
+            fragment = new Fragment_one();
+        } else if (position == 1) {
+            fragment = new Fragment_Two();
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if (position == 0) {
+            title = "Tab-1";
+        } else if (position == 1) {
+            title = "Tab-2";
+        }
+        return title;
+    }
+}
